03704d8c3e614439a150abf28386227be0d4411b
OrdepGCM <gamessforever18@gmail.com>
2021-10-23 16:55:39 -0300

    RESETTING PROJECT (html, css and js) ** Instead of creating move each side in the actions made, each piece of the "3d" cube will be made, hence the actions will move pieces.  *parts and function to rotate the finished parts. - RESETANDO PROJETO (html, css e js) ** Ao inves de mexer cada lado nas ações feitas, irá ser feito cada peça do cubo "3d", daí as ações vão mexer as peças.  * peças e função de girar prontas.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
-let bod = document.body
-let up = document.querySelector(".up")
-let left = document.querySelector(".left")
-let front = document.querySelector(".front")
-let rigth = document.querySelector(".rigth")
-let down = document.querySelector(".down")
-let U = document.querySelector(".U")
-let U_ = document.querySelector(".U-")
-let D = document.querySelector(".D")
-let D_ = document.querySelector(".D-")
-let L = document.querySelector(".L")
-let L_ = document.querySelector(".L-")
-let R = document.querySelector(".R")
-let R_ = document.querySelector(".R-")
+let cube = document.querySelector("#cube")
 let buttonU = document.querySelector(".buttonU")
 let buttonU_ = document.querySelector(".buttonU-")
 let buttonD = document.querySelector(".buttonD")
@@ -20,272 +7,163 @@ let buttonL = document.querySelector(".buttonL")
 let buttonL_ = document.querySelector(".buttonL-")
 let buttonR = document.querySelector(".buttonR")
 let buttonR_ = document.querySelector(".buttonR-")
+let piece1 = cube.querySelector(".piece1")
+let piece2 = cube.querySelector(".piece2")
+let piece3 = cube.querySelector(".piece3")
+let piece4 = cube.querySelector(".piece4")
+let piece5 = cube.querySelector(".piece5")
+let piece6 = cube.querySelector(".piece6")
+let piece7 = cube.querySelector(".piece7")
+let piece8 = cube.querySelector(".piece8")
+let piece9 = cube.querySelector(".piece9")
+let piece10 = cube.querySelector(".piece10")
+let piece11 = cube.querySelector(".piece11")
+let piece12 = cube.querySelector(".piece12")
+let piece13 = cube.querySelector(".piece13")
+let piece14 = cube.querySelector(".piece14")
+let piece15 = cube.querySelector(".piece15")
+let piece16 = cube.querySelector(".piece16")
+let piece17 = cube.querySelector(".piece17")
+let piece18 = cube.querySelector(".piece18")
+let piece19 = cube.querySelector(".piece19")
+let piece20 = cube.querySelector(".piece20")
+let piece21 = cube.querySelector(".piece21")
+let piece22 = cube.querySelector(".piece22")
+let piece23 = cube.querySelector(".piece23")
+let piece24 = cube.querySelector(".piece24")
+let piece25 = cube.querySelector(".piece25")
+let piece26 = cube.querySelector(".piece26")
+let piece27 = cube.querySelector(".piece27")
 
-let Up = {
-  square1: up.querySelector(".square1"),
-  square2: up.querySelector(".square2"),
-  square3: up.querySelector(".square3"),
-  square4: up.querySelector(".square4"),
-  square5: up.querySelector(".square5"),
-  square6: up.querySelector(".square6"),
-  square7: up.querySelector(".square7"),
-  square8: up.querySelector(".square8"),
-  square9: up.querySelector(".square9"),
-}
-let Left = {
-  square1: left.querySelector(".square1"),
-  square2: left.querySelector(".square2"),
-  square3: left.querySelector(".square3"),
-  square4: left.querySelector(".square4"),
-  square5: left.querySelector(".square5"),
-  square6: left.querySelector(".square6"),
-  square7: left.querySelector(".square7"),
-  square8: left.querySelector(".square8"),
-  square9: left.querySelector(".square9"),
-}
-let Front = {
-  square1: front.querySelector(".square1"),
-  square2: front.querySelector(".square2"),
-  square3: front.querySelector(".square3"),
-  square4: front.querySelector(".square4"),
-  square5: front.querySelector(".square5"),
-  square6: front.querySelector(".square6"),
-  square7: front.querySelector(".square7"),
-  square8: front.querySelector(".square8"),
-  square9: front.querySelector(".square9"),
-}
-let Rigth = {
-  square1: rigth.querySelector(".square1"),
-  square2: rigth.querySelector(".square2"),
-  square3: rigth.querySelector(".square3"),
-  square4: rigth.querySelector(".square4"),
-  square5: rigth.querySelector(".square5"),
-  square6: rigth.querySelector(".square6"),
-  square7: rigth.querySelector(".square7"),
-  square8: rigth.querySelector(".square8"),
-  square9: rigth.querySelector(".square9"),
-}
-let Down = {
-  square1: down.querySelector(".square1"),
-  square2: down.querySelector(".square2"),
-  square3: down.querySelector(".square3"),
-  square4: down.querySelector(".square4"),
-  square5: down.querySelector(".square5"),
-  square6: down.querySelector(".square6"),
-  square7: down.querySelector(".square7"),
-  square8: down.querySelector(".square8"),
-  square9: down.querySelector(".square9"),
-}
+let Up = [
+  piece1,
+  piece2,
+  piece3,
+  piece4,
+  piece5,
+  piece6,
+  piece7,
+  piece8,
+  piece9,
+]
+let Left = [
+  piece3,
+  piece6,
+  piece9,
+  piece12,
+  piece15,
+  piece18,
+  piece21,
+  piece24,
+  piece27,
+]
+let Front = [
+  piece1,
+  piece2,
+  piece3,
+  piece10,
+  piece12,
+  piece13,
+  piece19,
+  piece20,
+  piece21,
+]
+let Rigth = [
+  piece1,
+  piece4,
+  piece7,
+  piece10,
+  piece13,
+  piece16,
+  piece19,
+  piece22,
+  piece25,
+]
+let Back = [
+  piece7,
+  piece8,
+  piece9,
+  piece16,
+  piece17,
+  piece18,
+  piece25,
+  piece26,
+  piece27,
+]
+let Down = [
+  piece19,
+  piece20,
+  piece21,
+  piece22,
+  piece23,
+  piece24,
+  piece25,
+  piece26,
+  piece27,
+]
 
-function ElementsObjects(object, action, actionArg = "", squareMin = 1, squareMax = 9) {
-  if (squareMin == 1){
-    if(action == "classList.toggle")
-    object.square1.classList.toggle(actionArg)
-    else if(action == "transform")
-    object.square1.style.transform = actionArg
-  }
-  if (squareMin <= 2 && squareMax >= 2){
-    if(action == "classList.toggle")
-    object.square2.classList.toggle(actionArg)
-    else if(action == "transform")
-    object.square2.style.transform = actionArg
-  }
-  if (squareMin <= 3 && squareMax >= 3){
-    if(action == "classList.toggle")
-    object.square3.classList.toggle(actionArg)
-    else if(action == "transform")
-    object.square3.style.transform = actionArg
-  }
-  if (squareMin <= 4 && squareMax >= 4){
-    if(action == "classList.toggle")
-    object.square4.classList.toggle(actionArg)
-    else if(action == "transform")
-    object.square4.style.transform = actionArg
-  }
-  if (squareMin <= 5 && squareMax >= 5){
-    if(action == "classList.toggle")
-    object.square5.classList.toggle(actionArg)
-    else if(action == "transform")
-    object.square5.style.transform = actionArg
-  }
-  if (squareMin <= 6 && squareMax >= 6){
-    if(action == "classList.toggle")
-    object.square6.classList.toggle(actionArg)
-    else if(action == "transform")
-    object.square6.style.transform = actionArg
-  }
-  if (squareMin <= 7 && squareMax >= 7){
-    if(action == "classList.toggle")
-    object.square7.classList.toggle(actionArg)
-    else if(action == "transform")
-    object.square7.style.transform = actionArg
-  }
-  if (squareMin <= 8 && squareMax >= 8){
-    if(action == "classList.toggle")
-    object.square8.classList.toggle(actionArg)
-    else if(action == "transform")
-    object.square8.style.transform = actionArg
-  }
-  if (squareMin <= 9 && squareMax >= 9){
-    if(action == "classList.toggle")
-    object.square9.classList.toggle(actionArg)
-    else if(action == "transform")
-    object.square9.style.transform = actionArg
-  }
-  return ""
-}
-ElementsObjects(Up,"classList.toggle", "hiddenV")
-ElementsObjects(Left,"classList.toggle", "hiddenH")
-ElementsObjects(Rigth,"classList.toggle", "hiddenH")
-ElementsObjects(Down,"classList.toggle", "hiddenV")
+/*
+     if () {
+      backup[0] = Up[2]
+      backup[1] = Up[5]
+      backup[2] = Up[8]
+      backup[3] = Up[1]
+      backup[4] = Up[4]
+      backup[5] = Up[7]
+      backup[6] = Up[0]
+      backup[7] = Up[3]
+      backup[8] = Up[6]
+    }/*
+    for (var i = 0; i <=8; i++) {
+      Up[i] = backup[i]
+    }*/
 
+function rotatePieces(face, x = 0, y = 0, z = 0) {
+  var backup = []
+  var i = 0
+  face.forEach(piece => {
+    var oiu = new DOMMatrix(window.getComputedStyle(piece).getPropertyValue('transform'))
+
+    let bc = oiu.rotate(x, y, z).toJSON()
+    piece.style.transform = "matrix3d(" + bc.m11 + "," + bc.m12 + "," + bc.m13 + "," + bc.m14 + "," + bc.m21 + "," + bc.m22 + "," + bc.m23 + "," + bc.m24 + "," + bc.m31 + "," + bc.m32 + "," + bc.m33 + "," + bc.m34 + "," + bc.m41 + "," + bc.m42 + "," + bc.m43 + "," + bc.m44 + ")"
+    backup[i] = piece
+    i++
+  })
+  switch (face) {
+    case Up:
+      face[0] = backup[2]
+      face[1] = backup[5]
+      face[2] = backup[8]
+      face[3] = backup[1]
+      face[4] = backup[4]
+      face[5] = backup[7]
+      face[6] = backup[0]
+      face[7] = backup[3]
+      face[8] = backup[6]
+      break;
+  }
+}
 buttonU.onclick = () => {
-  if (!buttonU_.disabled) {
-    ElementsObjects(Rigth,"classList.toggle", "hiddenH", 1, 3)
-    ElementsObjects(Rigth,"transform","translateX(-180px)", 1,3)
-    ElementsObjects(Front,"classList.toggle", "hiddenH", 1, 3)
-    buttonU.disabled = true
-  }
-  else {
-    ElementsObjects(Left,"classList.toggle", "hiddenH", 1, 3)
-    ElementsObjects(Left,"transform","translateX(0px)", 1,3)
-    ElementsObjects(Front,"classList.toggle", "hiddenH", 1, 3)
-    buttonU_.disabled = false
-  }
+  rotatePieces(Up, 0, -90)
 }
 buttonU_.onclick = () => {
-  if (!buttonU.disabled) {
-    ElementsObjects(Left,"classList.toggle", "hiddenH", 1, 3)
-    ElementsObjects(Left,"transform","translateX(180px)", 1,3)
-    ElementsObjects(Front,"classList.toggle", "hiddenH", 1, 3)
-    buttonU_.disabled = true
-  }
-  else {
-    ElementsObjects(Rigth,"classList.toggle", "hiddenH", 1, 3)
-    ElementsObjects(Rigth,"transform","translateX(0px)", 1,3)
-    ElementsObjects(Front,"classList.toggle", "hiddenH", 1, 3)
-    buttonU.disabled = false
-  }
+  rotatePieces(Up, 0, 90)
 }
 buttonD.onclick = () => {
-  if (!buttonD_.disabled) {
-    ElementsObjects(Rigth, "hiddenH", 7)
-    ElementsObjects(Front, "hiddenH", 7)
-    buttonD.disabled = true
-  }
-  else {
-    ElementsObjects(Left, "hiddenH", 7)
-    ElementsObjects(Front, "hiddenH", 7)
-    buttonD_.disabled = false
-  }
+  rotatePieces(Down, 0, -90)
 }
 buttonD_.onclick = () => {
-  if (!buttonD.disabled) {
-    ElementsObjects(Left, "hiddenH", 7)
-    ElementsObjects(Front, "hiddenH", 7)
-    buttonD_.disabled = true
-  }
-  else {
-    ElementsObjects(Rigth, "hiddenH", 7)
-    ElementsObjects(Front, "hiddenH", 7)
-    buttonD.disabled = false
-  }
+  rotatePieces(Down, 0, 90)
 }
 buttonL.onclick = () => {
-  if (!buttonL_.disabled) {
-    ElementsObjects(Down, "hiddenV", 1, 1)
-    ElementsObjects(Down, "hiddenV", 4, 4)
-    ElementsObjects(Down, "hiddenV", 7, 7)
-    ElementsObjects(Front, "hiddenV", 1, 1)
-    ElementsObjects(Front, "hiddenV", 4, 4)
-    ElementsObjects(Front, "hiddenV", 7, 7)
-    buttonL.disabled = true
-  }
-  else {
-    ElementsObjects(Up, "hiddenV", 1, 1)
-    ElementsObjects(Up, "hiddenV", 4, 4)
-    ElementsObjects(Up, "hiddenV", 7, 7)
-    ElementsObjects(Front, "hiddenV", 1, 1)
-    ElementsObjects(Front, "hiddenV", 4, 4)
-    ElementsObjects(Front, "hiddenV", 7, 7)
-    buttonL_.disabled = false
-  }
+  rotatePieces(Left, -90)
 }
 buttonL_.onclick = () => {
-  if (!buttonL.disabled) {
-    ElementsObjects(Up, "hiddenV", 1, 1)
-    ElementsObjects(Up, "hiddenV", 4, 4)
-    ElementsObjects(Up, "hiddenV", 7, 7)
-    ElementsObjects(Front, "hiddenV", 1, 1)
-    ElementsObjects(Front, "hiddenV", 4, 4)
-    ElementsObjects(Front, "hiddenV", 7, 7)
-    buttonL_.disabled = true
-  }
-  else {
-    ElementsObjects(Down, "hiddenV", 1, 1)
-    ElementsObjects(Down, "hiddenV", 4, 4)
-    ElementsObjects(Down, "hiddenV", 7, 7)
-    ElementsObjects(Front, "hiddenV", 1, 1)
-    ElementsObjects(Front, "hiddenV", 4, 4)
-    ElementsObjects(Front, "hiddenV", 7, 7)
-    buttonL.disabled = false
-  }
+  rotatePieces(Left, 90)
 }
 buttonR.onclick = () => {
-  if (!buttonR_.disabled) {
-    ElementsObjects(Up, "hiddenV", 3, 3)
-    ElementsObjects(Up, "hiddenV", 6, 6)
-    ElementsObjects(Up, "hiddenV", 9, 9)
-    ElementsObjects(Front, "hiddenV", 3, 3)
-    ElementsObjects(Front, "hiddenV", 6, 6)
-    ElementsObjects(Front, "hiddenV", 9, 9)
-    buttonR.disabled = true
-  }
-  else {
-    ElementsObjects(Down, "hiddenV", 3, 3)
-    ElementsObjects(Down, "hiddenV", 6, 6)
-    ElementsObjects(Down, "hiddenV", 9, 9)
-    ElementsObjects(Front, "hiddenV", 3, 3)
-    ElementsObjects(Front, "hiddenV", 6, 6)
-    ElementsObjects(Front, "hiddenV", 9)
-    buttonR_.disabled = false
-  }
+  rotatePieces(Rigth, -90)
 }
 buttonR_.onclick = () => {
-  if (!buttonR.disabled) {
-    ElementsObjects(Down,"classList.toggle", "hiddenV", 3, 3)
-    ElementsObjects(Down,"classList.toggle", "hiddenV", 6, 6)
-    ElementsObjects(Down,"classList.toggle", "hiddenV", 9)
-    ElementsObjects(Front,"classList.toggle", "hiddenV", 3, 3)
-    ElementsObjects(Front,"classList.toggle", "hiddenV", 6, 6)
-    ElementsObjects(Front,"classList.toggle", "hiddenV", 9)
-    ElementsObjects(Down,"transform", "translateY(-180px)", 3, 3)
-    ElementsObjects(Down,"transform", "translateY(-180px)", 6, 6)
-    ElementsObjects(Down,"transform", "translateY(-180px)", 9)
-    ElementsObjects(Front,"transform", "translateY(-60px)", 6, 6)
-    ElementsObjects(Front,"transform", "translateY(-120px)", 9)
-    buttonR_.disabled = true
-  }
-  else {
-    ElementsObjects(Up, "hiddenV", 3, 3)
-    ElementsObjects(Up, "hiddenV", 6, 6)
-    ElementsObjects(Up, "hiddenV", 9, 9)
-    ElementsObjects(Front, "hiddenV", 3, 3)
-    ElementsObjects(Front, "hiddenV", 6, 6)
-    ElementsObjects(Front, "hiddenV", 9, 9)
-    buttonR.disabled = false
-  }
+  rotatePieces(Rigth, 90)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
