09b0797495bbb38f86ce5d6c632bf4a94903fb54
OrdepGCM <gamessforever18@gmail.com>
2021-10-24 23:00:13 -0300

    Update * updating face lists when a face is rotated - Atualização * atualizando as listas das faces quando uma face é girada

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ let buttonL = document.querySelector(".buttonL")
 let buttonL_ = document.querySelector(".buttonL-")
 let buttonR = document.querySelector(".buttonR")
 let buttonR_ = document.querySelector(".buttonR-")
+let buttonF = document.querySelector(".buttonF")
+let buttonF_ = document.querySelector(".buttonF-")
+let buttonB = document.querySelector(".buttonB")
+let buttonB_ = document.querySelector(".buttonB-")
 let piece1 = cube.querySelector(".piece1")
 let piece2 = cube.querySelector(".piece2")
 let piece3 = cube.querySelector(".piece3")
@@ -36,39 +40,39 @@ let piece26 = cube.querySelector(".piece26")
 let piece27 = cube.querySelector(".piece27")
 
 let Up = [
-  piece1,
-  piece2,
-  piece3,
-  piece4,
-  piece5,
-  piece6,
-  piece7,
-  piece8,
   piece9,
+  piece8,
+  piece7,
+  piece6,
+  piece5,
+  piece4,
+  piece3,
+  piece2,
+  piece1,
 ]
 let Left = [
-  piece3,
-  piece6,
   piece9,
-  piece12,
-  piece15,
+  piece6,
+  piece3,
   piece18,
-  piece21,
-  piece24,
+  piece15,
+  piece12,
   piece27,
+  piece24,
+  piece21,
 ]
 let Front = [
-  piece1,
-  piece2,
   piece3,
-  piece10,
+  piece2,
+  piece1,
   piece12,
-  piece13,
-  piece19,
-  piece20,
+  piece11,
+  piece10,
   piece21,
+  piece20,
+  piece19,
 ]
-let Rigth = [
+let Right = [
   piece1,
   piece4,
   piece7,
@@ -91,57 +95,179 @@ let Back = [
   piece27,
 ]
 let Down = [
-  piece19,
-  piece20,
   piece21,
-  piece22,
-  piece23,
+  piece20,
+  piece19,
   piece24,
-  piece25,
-  piece26,
+  piece23,
+  piece22,
   piece27,
+  piece26,
+  piece25,
 ]
 
-/*
-     if () {
-      backup[0] = Up[2]
-      backup[1] = Up[5]
-      backup[2] = Up[8]
-      backup[3] = Up[1]
-      backup[4] = Up[4]
-      backup[5] = Up[7]
-      backup[6] = Up[0]
-      backup[7] = Up[3]
-      backup[8] = Up[6]
-    }/*
-    for (var i = 0; i <=8; i++) {
-      Up[i] = backup[i]
-    }*/
-
 function rotatePieces(face, x = 0, y = 0, z = 0) {
+  document.querySelectorAll("button").forEach(button => {
+    button.disabled = true
+  })
+  xx=x
+  yy=y
+  zz=z
+  console.log(xx+"/"+yy+"/"+zz);
   var backup = []
   var i = 0
   face.forEach(piece => {
     var oiu = new DOMMatrix(window.getComputedStyle(piece).getPropertyValue('transform'))
+    if (Math.round((Math.acos(oiu.d) * (180 / Math.PI)) == 90)) {
+      if (oiu.m23 == 1 && oiu.m32 == -1) {
+        cc = -y
+        yy=z
+        zz=cc
+        //bc = oiu.rotate(x, z, -y).toJSON()
+        console.log(xx+"/"+yy+"/"+zz+"a");
+        
+      }
+      else if (oiu.m23 == -1 && oiu.m32 == 1) {
+        cc=y
+        yy=-z
+        zz=cc
+        console.log(xx+"/"+yy+"/"+zz);
+      }
+    }
+    else if (Math.round((Math.acos(oiu.d) * (180 / Math.PI)) == 180)) {
+      
+      let xx=x
+      let yy=y
+      let zz=z
+      console.log(xx+"/"+yy+"/"+zz);
+    }
+    else {
+      console.log();
+      bc = oiu.rotate(xx, yy, zz).toJSON()
+    }
 
-    let bc = oiu.rotate(x, y, z).toJSON()
     piece.style.transform = "matrix3d(" + bc.m11 + "," + bc.m12 + "," + bc.m13 + "," + bc.m14 + "," + bc.m21 + "," + bc.m22 + "," + bc.m23 + "," + bc.m24 + "," + bc.m31 + "," + bc.m32 + "," + bc.m33 + "," + bc.m34 + "," + bc.m41 + "," + bc.m42 + "," + bc.m43 + "," + bc.m44 + ")"
     backup[i] = piece
+    if (i == 0) {
+            //console.log(bc);
+            //console.log(bc.d+"/"+Math.round(Math.acos(bc.d) * (180/Math.PI)))
+    }
     i++
+
   })
+  if ((face == Up && y == -90) || (face == Left && x == -90) || (face == Front && z == 90) || (face == Right && x == 90) || (face == Back && z == -90) || (face == Down && y == 90)) {
+    face[0] = backup[6]
+    face[1] = backup[3]
+    face[2] = backup[0]
+    face[3] = backup[7]
+    face[4] = backup[4]
+    face[5] = backup[1]
+    face[6] = backup[8]
+    face[7] = backup[5]
+    face[8] = backup[2]
+  }
+  else if ((face == Up && y == 90) || (face == Left && x == 90) || (face == Front && z == -90) || (face == Right && x == -90) || (face == Back && z == 90) || (face == Down && y == -90)) {
+    face[0] = backup[2]
+    face[1] = backup[5]
+    face[2] = backup[8]
+    face[3] = backup[1]
+    face[4] = backup[4]
+    face[5] = backup[7]
+    face[6] = backup[0]
+    face[7] = backup[3]
+    face[8] = backup[6]
+  }
   switch (face) {
     case Up:
-      face[0] = backup[2]
-      face[1] = backup[5]
-      face[2] = backup[8]
-      face[3] = backup[1]
-      face[4] = backup[4]
-      face[5] = backup[7]
-      face[6] = backup[0]
-      face[7] = backup[3]
-      face[8] = backup[6]
+      Left[0] = face[0]
+      Left[1] = face[3]
+      Left[2] = face[6]
+      Front[0] = face[6]
+      Front[1] = face[7]
+      Front[2] = face[8]
+      Right[0] = face[8]
+      Right[1] = face[5]
+      Right[2] = face[2]
+      Back[0] = face[2]
+      Back[1] = face[1]
+      Back[2] = face[0]
+      break;
+    case Left:
+      Up[0] = face[0]
+      Up[3] = face[1]
+      Up[6] = face[2]
+      Front[0] = face[2]
+      Front[3] = face[5]
+      Front[6] = face[8]
+      Back[2] = face[0]
+      Back[5] = face[3]
+      Back[8] = face[6]
+      Down[0] = face[6]
+      Down[3] = face[7]
+      Down[6] = face[8]
+      break;
+    case Front:
+      Up[6] = face[0]
+      Up[7] = face[1]
+      Up[8] = face[2]
+      Left[2] = face[0]
+      Left[5] = face[3]
+      Left[8] = face[6]
+      Right[0] = face[2]
+      Right[3] = face[5]
+      Right[6] = face[8]
+      Down[0] = face[6]
+      Down[1] = face[7]
+      Down[2] = face[8]
+      break;
+    case Right:
+      Up[2] = face[0]
+      Up[5] = face[1]
+      Up[8] = face[2]
+      Front[2] = face[0]
+      Front[5] = face[3]
+      Front[8] = face[6]
+      Back[0] = face[2]
+      Back[3] = face[5]
+      Back[6] = face[8]
+      Down[2] = face[6]
+      Down[5] = face[7]
+      Down[8] = face[8]
+      break;
+    case Back:
+      Up[0] = face[2]
+      Up[1] = face[1]
+      Up[2] = face[0]
+      Left[0] = face[2]
+      Left[3] = face[5]
+      Left[6] = face[8]
+      Right[2] = face[0]
+      Right[5] = face[3]
+      Right[8] = face[6]
+      Down[6] = face[8]
+      Down[7] = face[7]
+      Down[8] = face[6]
+      break;
+    case Down:
+      Left[6] = face[0]
+      Left[7] = face[3]
+      Left[8] = face[6]
+      Front[6] = face[0]
+      Front[7] = face[1]
+      Front[8] = face[2]
+      Right[6] = face[2]
+      Right[7] = face[5]
+      Right[8] = face[8]
+      Back[6] = face[6]
+      Back[7] = face[7]
+      Back[8] = face[8]
       break;
   }
+  setTimeout(() => {
+  document.querySelectorAll("button").forEach(button => {
+    button.disabled = false
+  })
+  },2500)
 }
 buttonU.onclick = () => {
   rotatePieces(Up, 0, -90)
@@ -150,10 +276,10 @@ buttonU_.onclick = () => {
   rotatePieces(Up, 0, 90)
 }
 buttonD.onclick = () => {
-  rotatePieces(Down, 0, -90)
+  rotatePieces(Down, 0, 90)
 }
 buttonD_.onclick = () => {
-  rotatePieces(Down, 0, 90)
+  rotatePieces(Down, 0, -90)
 }
 buttonL.onclick = () => {
   rotatePieces(Left, -90)
@@ -162,8 +288,27 @@ buttonL_.onclick = () => {
   rotatePieces(Left, 90)
 }
 buttonR.onclick = () => {
-  rotatePieces(Rigth, -90)
+  rotatePieces(Right, 90)
 }
 buttonR_.onclick = () => {
-  rotatePieces(Rigth, 90)
+  rotatePieces(Right, -90)
 }
+buttonF.onclick = () => {
+  rotatePieces(Front, 0, 0, 90)
+}
+buttonF_.onclick = () => {
+  rotatePieces(Front, 0, 0, -90)
+}
+buttonB.onclick = () => {
+  rotatePieces(Back, 0, 0, -90)
+}
+buttonB_.onclick = () => {
+  rotatePieces(Back, 0, 0, 90)
+}
+
+/*
+Math.acos(matrix.a) * (180/Math.PI);
+var rotationY = Math.asin(matrix.b) * (180/Math
+
+window.getComputedStyle(piece).getPropertyValue('transform')
+*/
